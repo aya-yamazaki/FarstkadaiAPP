2ea2170b17b842ca3e073ef698df7a339ad8f358
ayayan <ay.yamazaki.test@gmail.com>
2022-02-07 13:01:00 +0900

    Initial Commit

---
+++ FirstkadaiAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstkadaiAPP
+//
+//  Created by ay-yamazaki on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
